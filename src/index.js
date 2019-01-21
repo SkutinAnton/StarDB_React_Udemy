1b69ff103bc2b7e8728825a04e4e6c8646b6fd1f
SkutinAnton <Skutin_anton@mail.ru>
2019-01-21 21:49:12 +0300

    feat: add components

--- src/index.js
+++ src/index.js
@@ -1,45 +1,5 @@
-class SwapiService {
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
 
-  api = 'https://swapi.co/api/';
-
-  async getResource(url) {
-    const res = await fetch(`${this.api}${url}`);
-    if (!res.ok) {
-      throw new Error(`Could not fetch ${this.api}${url}, received ${res.status}`);
-    }
-    return await res.json();
-  }
-
-  async getAllPeople() {
-    const res = await this.getResource(`people`);
-    return res.results;
-  }
-
-  getPerson(id) {
-    return this.getResource(`people/${id}`);
-  }
-
-  async getAllPlanets() {
-    const res = await this.getResource(`planets`);
-    return res.results;
-  }
-
-  getPlanet(id) {
-    return this.getResource(`planets/${id}`);
-  }
-
-  async getAllStarships() {
-    const res = await this.getResource(`starships`);
-    return res.results;
-  }
-
-  getStarship(id) {
-    return this.getResource(`starships/${id}`);
-  }
-}
-
-const swapi = new SwapiService();
-
-swapi.getStarship(3).then(res => {
-  console.log('res', res)
-})
+ReactDOM.render(<App />, document.getElementById('root'));
